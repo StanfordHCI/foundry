75245453601b22de96a73ef63f9eb3ee3e457df3
negarrahmati <rahmati@stanford.edu>
2014-09-23 21:24:40 -0700

    worker can't see hire button.

--- app/assets/javascripts/authoring/task_modal.js
+++ app/assets/javascripts/authoring/task_modal.js
@@ -28,9 +28,12 @@ function showTaskOverview(groupNum){
 		$("#edit-save-task").css('display', '');
 		$("#edit-save-task").attr('onclick', 'editTaskOverview(true,'+groupNum+')');
 		$("#edit-save-task").html('Edit');
+
+
 	}
 	else{
 		$("#edit-save-task").css('display', 'none');
+		$("#hire-task").css('display','none');
 	}
 }
 
