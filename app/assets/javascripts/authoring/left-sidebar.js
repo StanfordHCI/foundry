d86e01297321b4fa7f78f212dde774886a78554d
Jare Fagbemi <ofagbemi@stanford.edu>
2014-10-20 17:26:38 -0700

    Positioned timeline and left sidebar on menu open / close

--- app/assets/javascripts/authoring/left-sidebar.js
+++ app/assets/javascripts/authoring/left-sidebar.js
@@ -4,33 +4,37 @@
  */
 
 $(document).ready(function() {
-  var menuButton = $('#foundry-header .menu-button');
-  var leftSidebar = $('#left-sidebar');
-  var timelineContainer = $('#timeline-container');
+  var menuButton = $("#foundry-header .menu-button");
+  var leftSidebar = $("#left-sidebar");
+  var timelineContainer = $("#timeline-container");
   
   // hover listeners
   menuButton.mouseenter(function() {
-    // if the menu's already extended, ignore
-    if($(this).hasClass('active')) return;
+    // if the menu"s already extended, ignore
+    if($(this).hasClass("active")) return;
     
-    leftSidebar.css('left', -(leftSidebar.width() - 20) + 'px');
+    leftSidebar.css("left", -(leftSidebar.width() - 20) + "px");
+    timelineContainer.css("left", "20px");
   });
   
   menuButton.mouseleave(function() {
-    // if the menu's already extended, ignore
-    if($(this).hasClass('active')) return;
+    // if the menu"s already extended, ignore
+    if($(this).hasClass("active")) return;
     
-    leftSidebar.css('left', -leftSidebar.width() + 'px');
+    leftSidebar.css("left", -leftSidebar.width() + "px");
+    timelineContainer.css("left", 0);
   });
   
   // click listener
   menuButton.click(function() {
-    if($(this).hasClass('active')) {
-      leftSidebar.css('left', -leftSidebar.width() + 'px');
-      $(this).removeClass('active');
+    if($(this).hasClass("active")) {
+      leftSidebar.css("left", -leftSidebar.width() + "px");
+      timelineContainer.css("left", 0);
+      $(this).removeClass("active");
     } else {
-      leftSidebar.css('left', 0);
-      $(this).addClass('active');
+      leftSidebar.css("left", 0);
+      timelineContainer.css("left", leftSidebar.width() + "px");
+      $(this).addClass("active");
     }
   });
 });
