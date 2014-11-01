5d07c5e902e8caed753d96889ed3cf03cb8f6799
dretelny <dretelny@stanford.edu>
2014-11-01 14:22:16 -0700

    fixed the issue causing the team to end when a task was completed

--- app/assets/javascripts/authoring/timeline.js
+++ app/assets/javascripts/authoring/timeline.js
@@ -430,12 +430,16 @@ function redrawTimeline() {
 
   //Get the latest time and team status, update x position of cursor
   cursor = timeline_svg.select(".cursor");
-  var latest_time;
+  
+  //NOTE from DR: I commented out the block of code below because it was raising an error when the timeline was loaded 
+  //and the same exact code is included in awareness.js
+  
+  /* var latest_time;
   if (in_progress){
       latest_time = (new Date).getTime();
   } else {
       latest_time = loadedStatus.latest_time;
-  }
+  }*/
   
   //Next line is commented out after disabling the ticker
   //cursor_details = positionCursor(flashTeamsJSON, latest_time);
