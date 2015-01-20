458d49ebc084acaaa8299e02211878e8d2e64e74
Daniela Retelny <dretelny@stanford.edu>
2015-01-20 15:34:37 -0800

    changed back the domain since it didn't make a difference

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['DEFAULT_URL'],
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
