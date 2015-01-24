cfe37dfeb102498ddf56156153b28cb0e46b0843
Tulsee Doshi <tulsee27@gmail.com>
2015-01-23 18:24:51 -0800

    ugh modal wont load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require ./bootstrap-editable.min
 //= require ./bootstrap-tour.min
 //= require ./authoring/load
+//= require ./authoring/training
 //= require ./authoring/timeline
 //= require ./authoring/events
 //= require ./authoring/members
