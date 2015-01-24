6881de06d7f0eb58a53586cd9c4305889fa2d1e0
Tulsee Doshi <tulsee27@gmail.com>
2015-01-24 00:07:53 -0800

    pc training modal complete

--- app/assets/javascripts/authoring/training.js
+++ app/assets/javascripts/authoring/training.js
@@ -1,11 +1,33 @@
 function openModal(){
 	console.log("Hits this");	
-	$('#myModal').modal('show');
-	$('.next').click(function(){
-	  var nextId = $(this).parents('.tab-pane').next().attr("id");
-	  $('[href=#'+nextId+']').tab('show');
-	})
-	$('.first').click(function(){
-	  $('#myWizard a:first').tab('show')
-	})
+	if ($("#password").val().indexOf("pc") == 0){
+		$('#pcModal').modal('show');
+		$('.next').click(function(){
+	  		var nextId = $(this).parents('.tab-pane').next().attr("id");
+	  		if (nextId == "step10"){
+	  			document.getElementById("pcCompleteBtn").disabled = false;	  		
+			}
+	  		$('[href=#'+nextId+']').tab('show');
+		})
+		$('.first').click(function(){
+	  		$('#pcWizard a:first').tab('show')
+		})
+	}
+	else if ($("#password").val().indexOf("wo") == 0){
+		console.log("IN THIS IF STATEMENT");
+		$('#workerModal').modal('show');
+		$('.next').click(function(){
+	  		var nextId = $(this).parents('.tab-pane').next().attr("id");
+	  		$('[href=#'+nextId+']').tab('show');
+		})
+		$('.first').click(function(){
+	  		$('#workerWizard a:first').tab('show')
+		})	
+	}
+	else if($("#password").val() == ""){
+		alert("You must enter a code to launch training");
+	}
+	else{
+		alert("I'm sorry. The code you entered does not match our records. Please wait to recieve a code from us.");
+	}
 }
