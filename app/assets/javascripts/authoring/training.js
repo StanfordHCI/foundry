ba031eccc37101999e30c36c2657461e0ca4de97
Tulsee Doshi <tulsee27@gmail.com>
2015-01-28 23:38:09 -0800

    youtube vids stop playing

--- app/assets/javascripts/authoring/training.js
+++ app/assets/javascripts/authoring/training.js
@@ -7,15 +7,20 @@ function openModal(){
 	  			document.getElementById("pcCompleteBtn").disabled = false;	  		
 			}
 	  		$('[href=#'+nextId+']').tab('show');
-		})
+    		$("#pcModal .tab-pane iframe").attr("src", $("#pcModal .tab-pane iframe").attr("src"));
+		});
 		$('.prev').click(function(){
 			var prevId = $(this).parents('.tab-pane').prev().attr("id");
 			console.log(prevId)
 			$('[href=#'+prevId+']').tab('show');
-		})
+			$("#pcModal .tab-pane iframe").attr("src", $("#pcModal .tab-pane iframe").attr("src"));
+		});
 		$('.first').click(function(){
 	  		$('#pcWizard a:first').tab('show')
-		})
+		});
+		$("#pcModal").on('hidden.bs.modal', function (e) {
+    		$("#pcModal .tab-pane iframe").attr("src", $("#pcModal .tab-pane iframe").attr("src"));
+		});
 	}
 	else if ($("#password").val().indexOf("wo") == 0){
 		$('#workerModal').modal('show');
@@ -25,15 +30,20 @@ function openModal(){
 	  			document.getElementById("woCompleteBtn").disabled = false;	  		
 			}
 	  		$('[href=#'+nextId+']').tab('show');
-		})
+	  		$("#workerModal .tab-pane iframe").attr("src", $("#workerModal .tab-pane iframe").attr("src"));
+		});
 		$('.prev').click(function(){
 			var prevId = $(this).parents('.tab-pane').prev().attr("id");
 			console.log(prevId)
 			$('[href=#'+prevId+']').tab('show');
-		})
+			$("#workerModal .tab-pane iframe").attr("src", $("#workerModal .tab-pane iframe").attr("src"));
+		});
+		$("#workerModal").on('hidden.bs.modal', function (e) {
+    		$("#workerModal .tab-pane iframe").attr("src", $("#workerModal .tab-pane iframe").attr("src"));
+		});
 		$('.first').click(function(){
 	  		$('#workerWizard a:first').tab('show')
-		})	
+		});	
 	}
 	else if($("#password").val() == ""){
 		alert("You must enter a code to launch training");
