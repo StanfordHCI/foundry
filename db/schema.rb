ee0ae8695417cb68b5c463162766f1d421fe82b8
ofagbemi <ofagbemi@stanford.edu>
2015-02-09 12:07:12 +0900

    Revert schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141021055050) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
