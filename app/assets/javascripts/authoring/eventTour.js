9ed49db726f7eba5be594767203d15a0a5aeb5ab
Tulsee Doshi <tulsee27@gmail.com>
2015-02-15 17:37:22 -0800

    works on button click

--- app/assets/javascripts/authoring/eventTour.js
+++ app/assets/javascripts/authoring/eventTour.js
@@ -10,6 +10,7 @@ $("#tourBtn2").click(function(){
 	for (i = 0; i < flashTeamsJSON["events"].length; i++){
     	if (isWorkerTask(flashTeamsJSON["events"][i])){
     		elemId = i+1;
+    		prevElem = i;
     		console.log(elemId);
     		break;
     	}
@@ -20,29 +21,41 @@ $("#tourBtn2").click(function(){
 	// {	orphan: true, 
 	{
 		orphan: true,
-		title: "<b>Customize the Events</b>",
-		content: "<div class='tour-content-wrapper'>Add details to each event so workers "
-		+"can easily complete their tasks. Add a title, assign workers, specify inputs and ouputs "
-		+"of the task, write a description, and specify questions for task documentation."
-		+"<img src='/assets/editTask.gif'> </img></div>"
+		title: "<b>Welcome to Foundry!</b>",
+		// content: flashTeamsJSON['projectoverview'],
+		content: "<div class='tour-content-wrapper'>Welcome to Foundry. We're here to help you get started!</br>"
+		+ "Read the Project Description below, and follow the next few steps in order to start your project."
+		+ "<hr></hr>" 
+		+ flashTeamsJSON['projectoverview']
+		+"</div>"
 		+"<nav class='popover-navigation'><div class='btn-group'>"
-		+"<button class='btn btn-default' data-role='prev'>« Prev</button>"
 		+"<button class='btn btn-default' data-role='next'>Next »</button></div>"
 		+"<button class='btn btn-default' data-role='end'>End tour</button></nav></div>",
 		template: "<div class='popover tour'><div class='arrow'></div><h3 class='popover-title'></h3>"
 		+"<div class='popover-content'></div>"
 	},
-	{	element: "#g_" + elemId,
-		title: "<b>Welcome to Foundry</b>",
+	{	element: "#g_" + prevElem,
+		title: "<b>The Previous Task</b>",
 		// backdrop: true,
 		html: true,
-		content: "Hello there"
+		content: "This is the task right before yours."
+		+ " You can click on the task to read about the details of the task, and the documentation information"
+		+ " that the worker has left for you."
+	},
+	{	element: "#g_" + elemId,
+		title: "<b>Your Task</b>",
+		// backdrop: true,
+		html: true,
+		content: "This is <b>YOUR</b> task. You can now end this tour, and <br/><b>click on the task rectangle</b>"
+		+ " to read about your task, and get started.</br></br>"
+		+ "Pay close attention to the task description, the 'inputs' (what other workers have handed off to you)" 
+		+ ", and the deliverables you are expected to create"
 	}
 	]
 	});
 	eventTour.init();
     eventTour.start(true);
-    // eventTour.goTo(0); //Always start tour at the first step
+    eventTour.goTo(0); //Always start tour at the first step
 });
 
 
