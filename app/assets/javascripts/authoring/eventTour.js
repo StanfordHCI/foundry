66f3af675d8e1aeaa9ebc30abdad3d47e6fd63e2
Tulsee Doshi <tulsee27@gmail.com>
2015-02-18 09:34:24 -0800

    eventTour update

--- app/assets/javascripts/authoring/eventTour.js
+++ app/assets/javascripts/authoring/eventTour.js
@@ -22,49 +22,51 @@ window.onload = function(){
 	    		break;
 	    	}
 	    }
-	    var eventTour = new Tour({
-		autoscroll: true,
-		steps: [
-		// {	orphan: true, 
-		{
-			orphan: true,
-			title: "<b>Welcome to Foundry!</b>",
-			// content: flashTeamsJSON['projectoverview'],
-			content: "<div class='tour-content-wrapper'>Welcome to Foundry. We're here to help you get started!</br>"
-			+ "Read the Project Description below, and follow the next few steps in order to start your project."
-			+ "<hr></hr>" 
-			+ pOverview
-			+"</div>"
-			+"<nav class='popover-navigation'><div class='btn-group'>"
-			+"<button class='btn btn-default' data-role='next'>Next »</button></div>"
-			+"<button class='btn btn-default' data-role='end'>End tour</button></nav></div>",
-			template: "<div class='popover tour'><div class='arrow'></div><h3 class='popover-title'></h3>"
-			+"<div class='popover-content'></div>"
-		},
-		{	element: "#g_" + prevElem,
-			title: "<b>The Previous Task</b>",
-			// backdrop: true,
-			html: true,
-			content: "This is the task right before yours."
-			+ " Before working, <b>click on the task</b> to read about the details of the task, and the documentation information"
-			+ " that the previous worker has left for you."
-			+ "</br>Pay close attention to the tasks that have handoffs (arrows) leading into your task."
-		},
-		{	element: "#g_" + elemId,
-			title: "<b>Your Task</b>",
-			// backdrop: true,
-			html: true,
-			content: "This is <b>YOUR</b> task. You can now end this tour, and <br/><b>click on the task rectangle</b>"
-			+ " to read about your task, and get started.</br></br>"
-			+ "Pay close attention to the task description, the 'inputs' (what other workers have handed off to you)" 
-			+ ", and the deliverables you are expected to create"
+	    if (elemId){
+		    var eventTour = new Tour({
+			autoscroll: true,
+			steps: [
+			// {	orphan: true, 
+			{
+				orphan: true,
+				title: "<b>Welcome to Foundry!</b>",
+				// content: flashTeamsJSON['projectoverview'],
+				content: "<div class='tour-content-wrapper'>Welcome to Foundry. We're here to help you get started!</br>"
+				+ "Read the Project Description below, and follow the next few steps in order to start your project."
+				+ "<hr></hr>" 
+				+ pOverview
+				+"</div>"
+				+"<nav class='popover-navigation'><div class='btn-group'>"
+				+"<button class='btn btn-default' data-role='next'>Next »</button></div>"
+				+"<button class='btn btn-default' data-role='end'>End tour</button></nav></div>",
+				template: "<div class='popover tour'><div class='arrow'></div><h3 class='popover-title'></h3>"
+				+"<div class='popover-content'></div>"
+			},
+			{	element: "#g_" + prevElem,
+				title: "<b>The Previous Task</b>",
+				// backdrop: true,
+				html: true,
+				content: "This is the task right before yours."
+				+ " Before working, <b>click on the task</b> to read about the details of the task, and the documentation information"
+				+ " that the previous worker has left for you."
+				+ "</br>Pay close attention to the tasks that have handoffs (arrows) leading into your task."
+			},
+			{	element: "#g_" + elemId,
+				title: "<b>Your Task</b>",
+				// backdrop: true,
+				html: true,
+				content: "This is <b>YOUR</b> task. You can now end this tour, and <br/><b>click on the task rectangle</b>"
+				+ " to read about your task, and get started.</br></br>"
+				+ "Pay close attention to the task description, the 'inputs' (what other workers have handed off to you)" 
+				+ ", and the deliverables you are expected to create"
+			}
+			]
+			});
+			eventTour.init();
+		    eventTour.start(true);
+		    eventTour.goTo(0); //Always start tour at the first step
+		    $.cookie('first','1',{expires: 1});
 		}
-		]
-		});
-		eventTour.init();
-	    eventTour.start(true);
-	    eventTour.goTo(0); //Always start tour at the first step
-	    $.cookie('first','1',{expires: 1});
 	}
 	else{
 		console.log("YAY COOKIE");
