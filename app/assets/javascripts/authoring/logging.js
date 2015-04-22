648c7f5fadba3258fce371f7b286c6d2d1fb5467
Daniela Retelny <dretelny@stanford.edu>
2015-04-22 16:22:02 -0700

    updated name of a parameter in logActivity function and commented out the console.log that listed all of the activities

--- app/assets/javascripts/authoring/logging.js
+++ app/assets/javascripts/authoring/logging.js
@@ -1,6 +1,6 @@
-function logActivity(updateType, activityName, time, currentUser, chatName, teamId, loadedStatus){
+function logActivity(updateType, activityName, time, currentUser, chatName, teamId, activityObject){
    
-    var activityJSON = JSON.stringify(loadedStatus);
+    var activityJSON = JSON.stringify(activityObject);
     var currentUser = JSON.stringify(currentUser);
 
     var url = '/activity_logs/log_update';
@@ -10,10 +10,10 @@ function logActivity(updateType, activityName, time, currentUser, chatName, team
         type: 'post',
         data: {"update_type": updateType, "activity_type": activityName, "act_tstamp": time, "current_user": currentUser, "chat_name": chatName, "team_id": teamId, "activity_json": activityJSON},
         success: function (result) {
-            console.log(activityName);
+            //console.log(activityName);
         },
         error: function (){
-            console.log('something went wrong!');
+            console.log('!');
         }
     });
 }
