ae53719bef6a50fbc82960e8fe1b9f025dbc6d06
Artyom <artyom.tsaplin@gmail.com>
2015-09-08 08:54:47 +0300

    Check role for Panels button.

--- app/assets/javascripts/authoring/pauseTeam.js
+++ app/assets/javascripts/authoring/pauseTeam.js
@@ -213,11 +213,13 @@ function updateRequestChangeModal(selected){
                 + 'Description of task: <br /> <textarea class="request-change-input input-block-level" rows="5" value="" placeholder="Description of task" id="event-request-description"></textarea>';
         disabled = false;
     }else if (selected == 'taskWarmHire'){
+        var panelsButtonContent = '<br />'
+        if(current_user == "Author"){panelsButtonContent = '<br /> <a href="/workers/index" target="_blank" class="btn btn-default">Panels</a> <br />'}
         content = 'Task Name: <input type="text" class="request-change-input input-xlarge" id="event-request-name" value="" placeholder="Task Name"> <br />'
                 + 'Duration: <input type="number" class="request-change-input" id="event-request-hours" value="" min="0" placeholder="00" style="margin-left: 10px; width:36px;"/> Hours' 
                 + '<input type = "number" class="request-change-input" id="event-request-minutes" value="" placeholder="00" style=" margin-left: 15px; width:36px" min="0" step="15" max="45"/> Minutes <br />' 
                 + 'Description of task: <br /> <textarea class="request-change-input input-block-level" rows="5" value="" placeholder="Description of task" id="event-request-description"></textarea>'
-                + '<br /> <a href="/workers/index" target="_blank" class="btn btn-default">Panels</a> <br />'
+                + panelsButtonContent
                 + 'Name of panel member: <input type = "text" class="request-change-input input-block-level" id="panel-member-name" value="" placeholder="Name of panel member that you would like to hire."/><br />'
                 + 'Time Spend on Deciding: <input type = "text" class="request-change-input input-block-level" id="time-spend-deciding" value="" placeholder="Active Time Spend on Deciding on Panel Member (for our experimental logs)."/><br />';
         disabled = false;
