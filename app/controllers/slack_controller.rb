ab00e237ac301a6b8c9782446de22c57a78398e7
Jay H Patel <jaypatelh@gmail.com>
2017-05-18 13:15:17 -0700

    temporarily add redirect uri to be foundry-app-dev

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -6,7 +6,8 @@ class SlackController < ApplicationController
     response = client.oauth_access(
       client_id: ENV['SLACK_APP_CLIENT_ID'],
       client_secret: ENV['SLACK_APP_CLIENT_SECRET'],
-      code: params[:code]
+      code: params[:code],
+      redirect_uri: "http://foundry-app-dev.herokuapp.com/slack/auth/"
     )
     flash[:slack_info] = response
     redirect_to params[:state]
